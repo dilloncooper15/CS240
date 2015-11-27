8145579c72e29e6d8b6ff724f9c89eab249b7cbd
Dillon <dcooper@olivetcollege.edu>
2015-11-27 16:59:05 -0500

    Updated project

--- dcooper_lab10_ex4.py
+++
@@ -1,7 +0,0 @@
-# Dillon J. Cooper
-# Lab 10
-
-# Chapter 16, Exercise 4: A DNA sequence is a sting made up of As, Ts, Cs, and
-# Gs. Write a GUI application in which a DNA sequence is entered, and when the
-# Count button is clicked, the number of As, Ts, Cs, and Gs are counted and
-# displayed in the window.
--- dcooper_lab10_ex5.py
+++
@@ -1,6 +0,0 @@
-# Dillon J. Cooper
-# Lab 10
-
-# Chapter 16, Exercise 5: Write a GUI application that when a value is entered
-# in the text field and the Convert button is clicked, the value should be
-# converted from Fahrenheit to Celsius and displayed in the window.
