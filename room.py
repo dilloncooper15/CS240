019ebe471629c44c1deeef6399bb3fd971d7d62f
Dillon <dcooper@olivetcollege.edu>
2015-11-27 20:46:19 -0500

    Updated project

--- room.py
+++ room.py
@@ -41,7 +41,11 @@ class Room():
         self.visited = False
         for item in inv:
             self.inv.append(Item(**item))
-            self.flavorsentences.append(str(Item(**item)))
+            self.flavorsentences.append(Item(**item))
+            # for i in self.flavorsentences:
+            #     self.flavorsentences += str(i) + " "
+            #     self.flavorsentences.append(Item(**item))
+            # self.flavorsentences = self.flavorsentences[:-1]
 
     def _neighbor(self, direction):
         if direction in self.neighbors:
